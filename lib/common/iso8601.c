ccb9560d4ff76eba7349128f1cd26e6a9233acd4
Reid Wahl <nrwahl@protonmail.com>
2026-04-30 23:13:57 -0700

    API: libcrmcommon: Deprecate crm_time_subtract()
    External callers have no need to subtract from a crm_time_t object.
    Pacemaker should not be used for general-purpose date/time manipulation.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/iso8601.c
+++ lib/common/iso8601.c
@@ -1712,17 +1712,6 @@ pcmk__time_subtract(const crm_time_t *dt, const crm_time_t *value)
     return subtract_time(dt, value, false);
 }
 
-crm_time_t *
-crm_time_subtract(const crm_time_t *dt, const crm_time_t *value)
-{
-    if ((dt == NULL) || (value == NULL)) {
-        errno = EINVAL;
-        return NULL;
-    }
-
-    return pcmk__time_subtract(dt, value);
-}
-
 /*!
  * \internal
  * \brief Compare two time objects
@@ -2635,6 +2624,17 @@ crm_time_add(const crm_time_t *dt, const crm_time_t *value)
     }
 
     return pcmk__time_add(dt, value);
+}
+
+crm_time_t *
+crm_time_subtract(const crm_time_t *dt, const crm_time_t *value)
+{
+    if ((dt == NULL) || (value == NULL)) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    return pcmk__time_subtract(dt, value);
 }
 
 // LCOV_EXCL_STOP
