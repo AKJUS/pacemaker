700a9012924cd96cac0bbaed41d28c3d649317ab
Reid Wahl <nrwahl@protonmail.com>
2026-05-01 00:32:47 -0700

    API: libcrmcommon: Deprecate crm_time_add_months()
    External callers have no need to add months to a crm_time_t object.
    Pacemaker should not be used for general-purpose date/time manipulation.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/iso8601.c
+++ lib/common/iso8601.c
@@ -1568,6 +1568,52 @@ pcmk__copy_timet(time_t source_sec)
 
 /*!
  * \internal
+ * \brief Add a given number of months to a time object
+ *
+ * \param[in,out] dt     Time object
+ * \param[in]     value  Number of months to add (can be negative to subtract)
+ */
+static void
+add_months(crm_time_t *dt, int value)
+{
+    uint32_t year = 0;
+    uint32_t month = 0;
+    uint32_t day = 0;
+    int days_in_month = 0;
+
+    pcmk__time_get_ymd(dt, &year, &month, &day);
+
+    if (value > 0) {
+        for (int i = value; i > 0; i--) {
+            month++;
+            if (month == 13) {
+                month = 1;
+                year++;
+            }
+        }
+    } else {
+        for (int i = value; i < 0; i++) {
+            month--;
+            if (month == 0) {
+                month = 12;
+                year--;
+            }
+        }
+    }
+
+    days_in_month = days_in_month_year(month, year);
+
+    if (days_in_month < day) {
+        // Preserve day-of-month unless the month doesn't have enough days
+        day = days_in_month;
+    }
+
+    dt->years = year;
+    dt->days = get_ordinal_days(year, month, day);
+}
+
+/*!
+ * \internal
  * \brief Add one time object to another and return the sum
  *
  * \param[in] dt     Time object to add to
@@ -1592,7 +1638,7 @@ pcmk__time_add(const crm_time_t *dt, const crm_time_t *value)
     utc = copy_time_to_utc(value);
 
     crm_time_add_years(answer, utc->years);
-    crm_time_add_months(answer, utc->months);
+    add_months(answer, utc->months);
     pcmk__time_add_days(answer, utc->days);
     pcmk__time_add_seconds(answer, utc->seconds);
 
@@ -1714,7 +1760,7 @@ component_fn(enum pcmk__time_component component)
             return crm_time_add_years;
 
         case pcmk__time_months:
-            return crm_time_add_months;
+            return add_months;
 
         case pcmk__time_weeks:
             return add_weeks;
@@ -1798,10 +1844,10 @@ subtract_time(const crm_time_t *dt1, const crm_time_t *dt2, bool as_duration)
     crm_time_add_years(result, -utc->years);
 
     if (utc->months == INT_MIN) {
-        crm_time_add_months(result, -1);
+        add_months(result, -1);
         utc->months++;
     }
-    crm_time_add_months(result, -utc->months);
+    add_months(result, -utc->months);
 
     if (utc->days == INT_MIN) {
         pcmk__time_add_days(result, -1);
@@ -1925,45 +1971,6 @@ done:
     free(utc1);
     free(utc2);
     return rc;
-}
-
-void
-crm_time_add_months(crm_time_t *dt, int value)
-{
-    uint32_t year = 0;
-    uint32_t month = 0;
-    uint32_t day = 0;
-    int days_in_month = 0;
-
-    pcmk__time_get_ymd(dt, &year, &month, &day);
-
-    if (value > 0) {
-        for (int i = value; i > 0; i--) {
-            month++;
-            if (month == 13) {
-                month = 1;
-                year++;
-            }
-        }
-    } else {
-        for (int i = value; i < 0; i++) {
-            month--;
-            if (month == 0) {
-                month = 12;
-                year--;
-            }
-        }
-    }
-
-    days_in_month = days_in_month_year(month, year);
-
-    if (days_in_month < day) {
-        // Preserve day-of-month unless the month doesn't have enough days
-        day = days_in_month;
-    }
-
-    dt->years = year;
-    dt->days = get_ordinal_days(year, month, day);
 }
 
 void
@@ -2705,6 +2712,12 @@ void
 crm_time_add_weeks(crm_time_t *dt, int value)
 {
     add_weeks(dt, value);
+}
+
+void
+crm_time_add_months(crm_time_t *dt, int value)
+{
+    add_months(dt, value);
 }
 
 // LCOV_EXCL_STOP
