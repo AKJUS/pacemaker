AKJUS/pacemaker

commit 917a568c81b64d04c8794b8897669e8b3ee6e8d7

Refactor: libcrmcommon: Drop side effect from pcmk__marked_as_deleted()
And make it static.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/common/xml.c
@@ -534,6 +534,26 @@ pcmk__xml_position(const xmlNode *xml, enum pcmk__xml_flags ignore_if_set)
 
 /*!
  * \internal
+ * \brief Check whether an attribute is marked as deleted
+ *
+ * \param[in] attr       XML attribute
+ * \param[in] user_data  Ignored
+ *
+ * \return \c true if \c pcmk__xf_deleted is set for \p attr, or \c false
+ *         otherwise
+ *
+ * \note This is compatible with \c pcmk__xe_remove_matching_attrs().
+ */
+static bool
+marked_as_deleted(xmlAttr *attr, void *user_data)
+{
+    const xml_node_private_t *nodepriv = attr->_private;
+
+    return pcmk__is_set(nodepriv->flags, pcmk__xf_deleted);
+}
+
+/*!
+ * \internal
  * \brief Remove all attributes marked as deleted from an XML node
  *
  * \param[in,out] xml        XML node whose deleted attributes to remove
@@ -547,7 +567,7 @@ static bool
 commit_attr_deletions(xmlNode *xml, void *user_data)
 {
     pcmk__xml_reset_node_flags(xml, NULL);
-    pcmk__xe_remove_matching_attrs(xml, true, pcmk__marked_as_deleted, NULL);
+    pcmk__xe_remove_matching_attrs(xml, true, marked_as_deleted, NULL);
     return true;
 }
 

lib/common/crmcommon_private.h
@@ -173,9 +173,6 @@ G_GNUC_INTERNAL
 void pcmk__mark_xml_node_dirty(xmlNode *xml);
 
 G_GNUC_INTERNAL
-bool pcmk__marked_as_deleted(xmlAttrPtr a, void *user_data);
-
-G_GNUC_INTERNAL
 bool pcmk__dump_xml_attr(const xmlAttr *attr, void *user_data);
 
 G_GNUC_INTERNAL

lib/common/xml_attr.c
@@ -113,19 +113,6 @@ pcmk__mark_xml_attr_dirty(xmlAttr *a)
     pcmk__mark_xml_node_dirty(parent);
 }
 
-// This also clears attribute's flags if not marked as deleted
-bool
-pcmk__marked_as_deleted(xmlAttrPtr a, void *user_data)
-{
-    xml_node_private_t *nodepriv = a->_private;
-
-    if (pcmk__is_set(nodepriv->flags, pcmk__xf_deleted)) {
-        return true;
-    }
-    nodepriv->flags = pcmk__xf_none;
-    return false;
-}
-
 /*!
  * \internal
  * \brief Append an XML attribute to a buffer

commit 1c93425bd311686e27d0a868f38c68468e1e37fc

Fix: libcrmcommon: Clear remaining attrs' flags in commit_attr_deletions
This fixes a regression introduced by 917a568. This has not made it into
any release.

Commit 917a568 dropped a side effect from marked_as_deleted(): it no
longer resets flags for an attribute that is not marked as deleted.
Somehow this causes the Reattach cts-lab test to fail.

This commit fixes the regression by putting the flag clearing in the
caller (commit_attr_deletions()).

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/common/xml.c
@@ -601,9 +601,30 @@ marked_as_deleted(const xmlAttr *attr, void *user_data)
 
 /*!
  * \internal
- * \brief Remove all attributes marked as deleted from an XML node
+ * \brief Clear flags on an XML attribute
  *
- * \param[in,out] xml        XML node whose deleted attributes to remove
+ * \param[in,out] xml        XML attribute whose flags to reset
+ * \param[in,out] user_data  Ignored
+ *
+ * \return \c true (to continue iterating)
+ *
+ * \note This is compatible with \c pcmk__xe_foreach_attr().
+ */
+static bool
+reset_attr_flags(xmlAttr *attr, void *user_data)
+{
+    reset_node_flags((xmlNode *) attr, user_data);
+    return true;
+}
+
+/*!
+ * \internal
+ * \brief Commit an XML node's attribute deletions
+ *
+ * This clears the node's flags, deletes any of its attributes that have the
+ * \c pcmk__xf_deleted flag set, and clears the remaining attributes' flags.
+ *
+ * \param[in,out] xml        XML node whose attribute deletions to commit
  * \param[in,out] user_data  Ignored
  *
  * \return \c true (to continue traversing the tree)
@@ -615,6 +636,7 @@ commit_attr_deletions(xmlNode *xml, void *user_data)
 {
     reset_node_flags(xml, NULL);
     pcmk__xe_remove_matching_attrs(xml, true, marked_as_deleted, NULL);
+    pcmk__xe_foreach_attr(xml, reset_attr_flags, NULL);
     return true;
 }
 
@@ -623,8 +645,8 @@ commit_attr_deletions(xmlNode *xml, void *user_data)
  * \brief Finalize all pending changes to an XML document and reset private data
  *
  * Clear the ACL user and all flags, unpacked ACLs, and deleted node records for
- * the document; clear all flags on each node in the tree; and delete any
- * attributes that are marked for deletion.
+ * the document; clear all flags on each node in the tree; delete any attributes
+ * that are marked for deletion; and clear flags for the remaining attributes.
  *
  * \param[in,out] doc  XML document
  *